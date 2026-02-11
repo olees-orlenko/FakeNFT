a8a582f224ea53db312fbacf60edda6b07233b94
Ignat  Rogachevich <rssxzp@icloud.com>
2026-02-11 22:51:57 +0300

    [Feature] Change color on actual and make dark theme support

--- Cart/Sort/SortMenuView.swift
+++ Cart/Sort/SortMenuView.swift
@@ -23,22 +23,21 @@ struct SortMenuView: View {
                     }
                 VStack(spacing: 0) {
                     VStack(spacing: 0) {
-                        Text("Title")
+                        Text(title)
                             .font(.caption2)
                             .foregroundStyle(Color.gray)
                             .padding(.vertical, 12)
-                        
+
                         Divider()
-                        
+
                         ForEach(Array(options.enumerated()), id: \.element.id) {
                             index, option in
-                            
+
                             Button(action: {
-                                withAnimation{ isShowingSortMenu = false
+                                withAnimation { isShowingSortMenu = false
                                     DispatchQueue.main.asyncAfter(deadline: .now() + 0.12) {
                                         option.action()
                                     }
-                                    
                                 }
                             }
                             ) {
@@ -46,7 +45,7 @@ struct SortMenuView: View {
                                     .frame(maxWidth: .infinity)
                                     .padding(.vertical, 18)
                             }
-                            
+
                             if index < options.count - 1 {
                                 Divider()
                             }
@@ -54,39 +53,51 @@ struct SortMenuView: View {
                     }
                     .background(
                         RoundedRectangle(cornerRadius: 13, style: .continuous)
-                            .fill(Color.mint) // Change color
+                            .fill(.menuBackground)
                     )
                     .padding(.horizontal, 8)
                     .padding(.bottom, 8)
-                    
-                    Button(action: {withAnimation{ isShowingSortMenu = false }
+
+                    Button(action: { withAnimation { isShowingSortMenu = false }
                     }) {
                         Text(closeButtonTitle)
                             .bold()
                             .frame(maxWidth: .infinity)
                             .padding()
                             .background(RoundedRectangle(cornerRadius: 13, style: .continuous)
-                                .fill(Color(UIColor.systemBackground))
+                                .fill(.blackAdaptive)
                             )
                     }
                     .padding(.horizontal, 8)
                 }
                 .transition(.move(edge: .bottom).combined(with: .opacity))
                 .zIndex(1)
-                .frame(maxWidth: .infinity,maxHeight: .infinity,alignment: .bottom)
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
             }
         }
-        .animation(.easeOut(duration: 0.22),value: isShowingSortMenu)
+        .animation(.easeOut(duration: 0.22), value: isShowingSortMenu)
     }
 }
 
-#Preview ("SortMenu") {
+#Preview("Sort Menu White") {
     @Previewable @State var isShowingSortMenu = true
     let options = [
-            SortOption(title: "По цене", action: { print("Сортировка по цене") }),
-            SortOption(title: "По названию", action: { print("Сортировка по названию") }),
-            SortOption(title: "По рейтингу", action: { print("Сортировка по рейтингу") })
-        ]
-    
-    SortMenuView(isShowingSortMenu: $isShowingSortMenu, title: "nav", options: options, closeButtonTitle: "Cancel")
+        SortOption(title: "По цене", action: { print("Сортировка по цене") }),
+        SortOption(title: "По названию", action: { print("Сортировка по названию") }),
+        SortOption(title: "По рейтингу", action: { print("Сортировка по рейтингу") }),
+    ]
+
+    SortMenuView(isShowingSortMenu: $isShowingSortMenu, title: "Сортировка", options: options, closeButtonTitle: "Закрыть")
+}
+
+#Preview("Sort Menu Black") {
+    @Previewable @State var isShowingSortMenu = true
+    let options = [
+        SortOption(title: "По цене", action: { print("Сортировка по цене") }),
+        SortOption(title: "По названию", action: { print("Сортировка по названию") }),
+        SortOption(title: "По рейтингу", action: { print("Сортировка по рейтингу") }),
+    ]
+
+    SortMenuView(isShowingSortMenu: $isShowingSortMenu, title: "Сортировка", options: options, closeButtonTitle: "Закрыть")
+        .preferredColorScheme(.dark)
 }
