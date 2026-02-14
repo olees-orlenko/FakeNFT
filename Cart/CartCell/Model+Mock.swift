ff39da4c340324f584497ba569198ff6e5c4a7a7
Ignat  Rogachevich <rssxzp@icloud.com>
2026-02-14 04:25:50 +0300

    [Refactor] Make id let constant and move MockData in enum

--- Cart/CartCell/Model+Mock.swift
+++ Cart/CartCell/Model+Mock.swift
@@ -8,13 +8,15 @@
 import Foundation
 
 struct CartModel: Identifiable {
-    var id = UUID()
+    let id = UUID()
     let name: String
     let image: String
-    let rating : Int
+    let rating: Int
     let price: Double
 }
 
-let cartMock = CartModel(name: "April", image: "April", rating: 3, price: 3.14)
-let cartMock2 = CartModel(name: "Greena", image: "Greena", rating: 4, price: 2.28)
-let cartMock3 = CartModel(name: "Spring", image: "Spring", rating: 5, price: 3.22)
+enum MockData {
+    static let cartMock = CartModel(name: "April", image: "April", rating: 3, price: 3.14)
+    static let cartMock2 = CartModel(name: "Greena", image: "Greena", rating: 4, price: 2.28)
+    static let cartMock3 = CartModel(name: "Spring", image: "Spring", rating: 5, price: 3.22)
+}
