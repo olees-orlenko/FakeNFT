9da3dd713ba57a731c22d5802f75801542c9347b
Ignat  Rogachevich <rssxzp@icloud.com>
2026-02-16 01:10:36 +0300

    [Refactor] Create PaymentSuccessView

---
+++ iOS-FakeNFT-Extended/Cart/Payment/PaymentSuccessView.swift
@@ -0,0 +1,43 @@
+//
+//  PaymentSuccessView.swift
+//  iOS-FakeNFT-Extended
+//
+//  Created by Игнат Рогачевич on 16.02.26.
+//
+
+import SwiftUI
+
+struct PaymentSuccessView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+
+            Image(.paymentSuccess)
+            Text("Успех! Оплата прошла,\n поздравляем с покупкой!")
+                .font(.system(size: 22, weight: .bold))
+                .foregroundStyle(.blackAdaptive)
+                .multilineTextAlignment(.center)
+                .lineLimit(2)
+                .frame(maxWidth: .infinity)
+                .padding(.horizontal, 36)
+
+            Spacer()
+
+            Button(action: {}) {
+                Text("Вернутся в корзину")
+                    .font(.system(size: 17, weight: .bold))
+                    .foregroundStyle(.whiteAdaptive)
+                    .padding()
+            }
+            .frame(maxWidth: .infinity, maxHeight: 60)
+            .background(RoundedRectangle(cornerRadius: 12).fill(.blackAdaptive))
+            .padding()
+        }
+        .background(.whiteAdaptive)
+    }
+}
+
+#Preview {
+    PaymentSuccessView()
+        .preferredColorScheme(.dark)
+}
