d4b78359f777c88af3f2da0296a189273fffa7ae
Ignat  Rogachevich <rssxzp@icloud.com>
2026-02-15 23:58:28 +0300

    [Feature] Create basic PaymentCell

--- iOS-FakeNFT-Extended/Cart/Payment/PaymentOptions.swift
+++ iOS-FakeNFT-Extended/Cart/Payment/PaymentOptions.swift
@@ -8,10 +8,18 @@
 import Foundation
 
 struct PaymentOptions {
-     let name: String
-     let icon: String
-    
-    
-    
-    
+    let name: String
+    let shortName: String
+    let icon: String
+}
+
+enum PaymentMethods {
+    static let bitcoin = PaymentOptions(name: "Bitcoin", shortName: "BTC", icon: "Bitcoin")
+    static let apeCoin = PaymentOptions(name: "ApeCoin", shortName: "APE", icon: "ApeCoin")
+    static let dogecoin = PaymentOptions(name: "Dogecoin", shortName: "DOGE", icon: "Dogecoin")
+    static let tether = PaymentOptions(name: "Tether", shortName: "USDT", icon: "Tether")
+    static let solana = PaymentOptions(name: "Solana", shortName: "SOL", icon: "Solana")
+    static let ethereum = PaymentOptions(name: "Ethereum", shortName: "ETH", icon: "Ethereum")
+    static let cardano = PaymentOptions(name: "Cardano", shortName: "ADA", icon: "Cardano")
+    static let shibaInu = PaymentOptions(name: "Shiba Inu", shortName: "ADA", icon: "Shiba Inu")
 }
