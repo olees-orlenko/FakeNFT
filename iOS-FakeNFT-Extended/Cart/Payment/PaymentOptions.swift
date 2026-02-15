4bb9a3f153ebe37f818a282e8f15fe16de89ca8d
Ignat  Rogachevich <rssxzp@icloud.com>
2026-02-15 23:40:10 +0300

    [Refcator] Change SortMenu icon

---
+++ iOS-FakeNFT-Extended/Cart/Payment/PaymentOptions.swift
@@ -0,0 +1,17 @@
+//
+//  PaymentOptions.swift
+//  iOS-FakeNFT-Extended
+//
+//  Created by Игнат Рогачевич on 15.02.26.
+//
+
+import Foundation
+
+struct PaymentOptions {
+     let name: String
+     let icon: String
+    
+    
+    
+    
+}
