8bbadbfc24eebb5b34f8e778c7bb777b9a7fef83
Ignat  Rogachevich <rssxzp@icloud.com>
2026-02-24 10:31:11 +0300

    [Feature] Replace PaymentOptions with Currency model

---
+++ iOS-FakeNFT-Extended/Cart/Models/Currency.swift
@@ -0,0 +1,21 @@
+//
+//  PaymentOptions.swift
+//  iOS-FakeNFT-Extended
+//
+//  Created by Игнат Рогачевич on 15.02.26.
+//
+
+import Foundation
+
+struct Currency: Codable, Identifiable {
+    let id: String
+    let title: String
+    let name: String
+    let imageURL: String
+}
+
+struct PaymentResponse: Codable {
+    let success: Bool
+    let id: String
+    let orderId: String
+}
