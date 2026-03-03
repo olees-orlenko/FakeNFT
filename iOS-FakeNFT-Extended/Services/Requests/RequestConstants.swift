7f17a5187b116119db8c5fead14ec7fd6aab6ee6
Ignat  Rogachevich <rssxzp@icloud.com>
2026-03-03 19:02:58 +0300

    feat: update token

--- iOS-FakeNFT-Extended/Services/Requests/RequestConstants.swift
+++ iOS-FakeNFT-Extended/Services/Requests/RequestConstants.swift
@@ -1,4 +1,4 @@
 enum RequestConstants {
     static let baseURL = "https://d5dn3j2ouj72b0ejucbl.apigw.yandexcloud.net"
-    static let token = "27e1bbac-7be8-4c09-9479-74586f95a085"
+    static let token = "8467b4e6-3044-42d9-8ce7-8b6c5a1882db"
 }
