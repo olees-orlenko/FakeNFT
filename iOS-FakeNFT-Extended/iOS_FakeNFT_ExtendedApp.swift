86b48980772ea0c4772ab1098cdb5b8fb310c584
Olesya Orlenko <olees.orlenko@yandex.ru>
2026-03-02 16:18:45 -0800

    [Refactor]remove unneeded files

--- iOS-FakeNFT-Extended/iOS_FakeNFT_ExtendedApp.swift
+++ iOS-FakeNFT-Extended/iOS_FakeNFT_ExtendedApp.swift
@@ -8,7 +8,6 @@ struct iOS_FakeNFT_ExtendedApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environment(ServicesAssembly(networkClient: DefaultNetworkClient(), nftStorage: NftStorageImpl()))
                 .environmentObject(favoritesManager)
                 .environmentObject(cartManager)
         }
